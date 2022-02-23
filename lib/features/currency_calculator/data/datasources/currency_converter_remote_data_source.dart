0fcf5454e174b320a3b4c565df9fc09a9c31bcbd
Fatai <adelefatai@yahoo.com>
2022-02-23 10:01:01 +0100

    Converter class for making api calls to server

---
+++ lib/features/currency_calculator/data/datasources/currency_converter_remote_data_source.dart
@@ -0,0 +1,30 @@
+import 'package:http/http.dart' as http;
+import '../../../../core/error/failure.dart';
+import '../models/converter_response.dart';
+
+///this class makes calls directly to the api
+abstract class ConverterRemoteDataSource {
+  ///gets results from endpoint and returns the model
+  ///or throws server exception if error
+  Future<ConverterResponse> getCurrencyRate();
+}
+
+class ConverterRemoteDataSourceImpl implements ConverterRemoteDataSource {
+  ConverterRemoteDataSourceImpl({required this.client});
+
+  final http.Client client;
+
+  @override
+  Future<ConverterResponse> getCurrencyRate() async {
+    final response = await client.get(
+      Uri.parse('http://data.fixer.io/api/latest?access_key=b641a6e48ddbb9bff685fe3b058136be&symbols=USD&format=1'),
+    );
+
+    if (response.statusCode == 200) {
+      var converterResponse = converterResponseFromJson(response.body);
+      return converterResponse;
+    } else {
+      throw ServerException();
+    }
+  }
+}
